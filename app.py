6adead8ad1d2f782062b8011cf5e4def2f921735
Marco Rodrigues <marcoacavaco.rodrigues@hotmail.com>
2023-03-14 13:09:50 +0100

    adding elements to form and structuruing html/css

--- app.py
+++ app.py
@@ -10,51 +10,52 @@ from forms.form_services.form_services import ServicesForm
 
 
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.abspath(__file__)) # to add attachments
-# app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
-# app.config['MAIL_PORT'] = 587
-# app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
-# app.config['MAIL_PASSWORD'] = os.getenv("PASS")
-# app.config['MAIL_USE_TLS'] = True  # more secure version of SSL.
-# app.config['MAIL_USE_SSL'] = False
+app.secret_key = "thisisasecretkey"   # You can use a random key
+app.config['UPLOAD_FOLDER'] = os.path.dirname(
+    os.path.abspath(__file__))  # to add attachments
+app.config['MAIL_SERVER'] = os.getenv("MAIL_SERVER")
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("PASS")
+app.config['MAIL_USE_TLS'] = True  # more secure version of SSL.
+app.config['MAIL_USE_SSL'] = False
 
-# mail = Mail(app)
-# Bootstrap(app)
-
-
-# def send_email_results():
-#     """Send email with all the inputs."""
-#     pass
+mail = Mail(app)
+Bootstrap(app)
 
 
-# def render_page(lang, html):
-#     """Render page. Takes the language and html file as inputs."""
-#     current_year = dt.datetime.now().year
-#     if form.is_submitted():  # if form is submitted
-#         if form.validate():  # if form is validated
-#             send_email_results()
-#             if lang == "pt":
-#                 return render_template("success_pt.html")
-#             if lang == "en":
-#                 return render_template("success_en.html")
-#             if lang == "es":
-#                 return render_template("success_es.html")
-#         else:
-#             if lang == "pt":
-#                 flash('*Alguns campos estão incompletos ou em falta.')
-#             if lang == "en":
-#                 flash('*Some fields are incomplete or missing.')
-#             if lang == "es":
-#                 flash('*Algunos campos están incompletos o faltan.')
-#     return render_template(html, year=current_year, form=form)
+def send_email_results():
+    """Send email with all the inputs."""
+    pass
+
+
+def render_page(lang, html):
+    """Render page. Takes the language and html file as inputs."""
+    form = PythonClassesForm()
+    current_year = dt.datetime.now().year
+    if form.is_submitted():  # if form is submitted
+        if form.validate():  # if form is validated
+            send_email_results()
+            if lang == "pt":
+                return render_template("success_pt.html")
+            if lang == "en":
+                return render_template("success_en.html")
+            if lang == "es":
+                return render_template("success_es.html")
+        else:
+            if lang == "pt":
+                flash('*Alguns campos estão incompletos ou em falta.')
+            if lang == "en":
+                flash('*Some fields are incomplete or missing.')
+            if lang == "es":
+                flash('*Algunos campos están incompletos o faltan.')
+    return render_template(html, year=current_year, form=form)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home_en():
     """Render the html in english language."""
-    # return render_page('en', html_en)
-    print('ok')
-    return render_template('index_python_en.html')
+    return render_page('en', 'index_python_en.html')
 
 
 # @app.route("/pt", methods=["GET", "POST"])
@@ -70,20 +71,4 @@ def home_en():
 
 
 if __name__ == '__main__':
-    # run application
-    # # select form
-    # form = PythonClassesForm()  # here select the form you whish to use
-    # if form == PythonClassesForm():
-    #     html_pt = 'index_python_pt.html'
-    #     html_en = 'index_python_en.html'
-    #     html_es = 'index_python_es.html'
-    # if form == PhotographyForm():
-    #     html_pt = 'index_photo_pt.html'
-    #     html_en = 'index_photo_en.html'
-    #     html_es = 'index_photo_es.html'
-    # if form == ServicesForm():
-    #     html_pt = 'index_services_pt.html'
-    #     html_en = 'index_services_en.html'
-    #     html_es = 'index_services_es.html'
-    # run application
     app.run(debug=True, port=7000)
--- forms/form_python_classes/form_python_classes.py
+++ forms/form_python_classes/form_python_classes.py
@@ -1,47 +1,158 @@
 from flask_wtf import FlaskForm
-from wtforms import widgets, StringField, SubmitField, DateField, DateTimeField
-from wtforms import TextAreaField, SelectField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, Regexp
+from wtforms import StringField, SubmitField
+from wtforms import TextAreaField, SelectField
+from wtforms.validators import DataRequired, Email, Regexp
 
+
+# VARIABLES
+ERROR_SELECT_OPTION = 'Please select an option.'
+FREGUESIAS = [
+            'Alter do Chão',
+            'Chancelaria',
+            'Cunheira',
+            'Seda',
+            'Arronches, Assunção',
+            'Esperança',
+            'Mosteiros',
+            'Avis',
+            'Ervedal',
+            'Aldeia Velha',
+            'Figueira e Barros',
+            'Campo Maior',
+            'Nossa Senhora da Graça dos Degolados',
+            'São João Baptista '
+            'Castelo de Vide',
+            'Póvoa e Meadas',
+            'Santa Maria da Devesa',
+            'Santiago Maior',
+            'São João Baptista',
+            'Crato e Mártires',
+            'Aldeia da Mata',
+            'Gáfete',
+            'Monte da Pedra',
+            'Elvas',
+            'Santa Eulália',
+            'São Brás e São Lourenço',
+            'São Vicente e Ventosa',
+            'Fronteira',
+            'Cabeço de Vide',
+            'São Saturnino',
+            'Gavião',
+            'Belver',
+            'Comenda',
+            'Margem',
+            'Marvão',
+            'Beirã',
+            'Santo António das Areias',
+            'São Salvador da Aramenha',
+            'Monforte',
+            'Assumar',
+            'Santo Aleixo',
+            'Vaiamonte', 
+            'Nisa', 
+            'Alpalhão',
+            'Montalvão',
+            'Santana',
+            'São Matias',
+            'Tolosa',
+            'Ponte de Sor',
+            'Foros de Arrão',
+            'Galveias',
+            'Longomel',
+            'Montargil',
+            'Portalegre',
+            'Alagoa',
+            'Fortios',
+            'Sousel',
+            'Cano',
+            'Casa Branca',
+            'Santo Amaro']
+FREGUESIAS.sort()
+EMAIL_REGEX = "(^$|^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$)"
 
 class PythonClassesForm(FlaskForm):
     """Class to launch Flask Form concerning Python Classes."""
-    purpose = SelectField(
-        'Why would you like to learn Python?',
-        choices=[
-            'Work',
-            'Personal projects',
-            'Curiosity',
-            'Other'
-        ],
-        validators=[DataRequired(message='Please select an option.')])
 
-    purpose_other = StringField("Please explain, in case you chose 'Other'.")
+    purpose = SelectField(
+        '1. Why would you like to learn Python?',
+        choices=['Work', 'Personal projects', 'Curiosity', 'Other'])
+
+    purpose_other = StringField(
+        "1.1. Please explain, in case you chose 'Other'.")
 
     hours_per_week = SelectField(
-        'How many hours a week would you like to have classes?',
-        choices=[
-            '1h - 2h',
-            '2h - 3h',
-            '3h - 4h',
-            '4h +'
-        ],
-        validators=[DataRequired(message='Please select an option.')])
-    
+        '2. How many hours a week would you like to have classes?',
+        choices=['1h - 2h', '2h - 3h', '3h - 4h', '4h +'])
+
     site_remote = SelectField(
-        'Would You rather have on-site classes or remotely?',
-        choices=[
-            'On-site',
-            'Remote',
-            'No preference'
-        ],
-        validators=[DataRequired(message='Please select an option.')])
-    
+        '3. Would You rather have on-site classes or remotely?',
+        choices=['On-site', 'Remote', 'No preference'])
+
     distance_onsite = SelectField(
-        """If you chose 'on-site', how many km would you be willing to do,
-          to attend the classes?""",
+        """3.1. If you chose 'on-site', 
+        how many km would you be willing to do?""",
+        choices=['0k-5km', '0k-10km', '0k-20km'])
+
+    subject = SelectField(
+        "4. Which subject would you prefer to master with Python?",
         choices=[
-            '0k-5km',
-            '0k-10km',
-            '0k-20km'
+            'Python basics',
+            "GUI's (Graphic User Interface) development",
+            'Data Anlaysis',
+            'Web Development',
+            'Other'
         ])
+
+    subject_other = StringField(
+        "4.1. Please explain, in case you chose 'Other'.")
+
+    pay = SelectField(
+        "5. Would You be willing to pay for having classes?",
+        choices=['Yes', 'No'])
+    
+    pay_how_much = SelectField(
+        "5.1. If 'Yes', how much would you be willing to pay?",
+        choices=[
+            '8-15€ per week',
+            '20-30€ per month',
+            '30-40€ per month',
+            '5-10€ per session (1-2 hours)',
+            'Other'
+        ])
+
+    pay_how_much_other = StringField(
+        "5.2. Please explain, if you chose 'Other' type of payment.")
+
+    pay_premium = SelectField(
+        """5.3. Would you be willing to pay a premium subscription,
+          for extra classes and customized support to build a portfolio?""",
+        choices=['Yes', 'No', 'Maybe'])
+
+    when = SelectField(
+        "6. When you rather have the classes?",
+        choices=['During the week', 'Weekends'])
+
+    when_hours = SelectField(
+        "6.1. At what time of the day?",
+        choices=['Morning', 'Afternoon', 'Evening'])
+
+    age = StringField("7. Can you please provide your age?")
+
+    location = SelectField(
+        "8. Can you please provide your location?",
+        choices=FREGUESIAS)
+
+    # email = StringField("9. If you would like to be contacted for more information, and be shortlisted for Python Classes, please provide your email address:",
+    #     validators=[Email(message='Please insert a valid email.')])
+
+    email = StringField(
+        """9. If you would like to be contacted for more 
+        information, please provide your email address.""",
+        validators=[
+            Regexp(
+                EMAIL_REGEX,
+                message='Please provide a valid email address.')])
+    
+    observations = TextAreaField("Please write below any observations:")
+
+    submit = SubmitField('Submit')
